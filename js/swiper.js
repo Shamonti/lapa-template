276942d052d971f2f796f819636cb4f8cc1d9ea8
Shamonti <67649277+Shamonti@users.noreply.github.com>
2021-09-15 17:39:13 +0600

    added fine__arts page

---
+++ js/swiper.js
@@ -0,0 +1,15 @@
+// import Swiper from 'swiper/swiper-bundle.esm.js';
+// import 'swiper/swiper-bundle.css';
+var swiper = new Swiper('.swiper', {
+  slidesPerView: 'auto',
+  spaceBetween: 30,
+  // pagination: {
+  //   el: '.swiper-pagination',
+  //   clickable: true,
+  // },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
