e08cce956be3ce23d7ae21aaa076013c20047404
Shamonti <67649277+Shamonti@users.noreply.github.com>
2021-11-16 17:04:20 +0600

    revision

--- js/swiper.js
+++ js/swiper.js
@@ -1,6 +1,6 @@
 // import Swiper from 'swiper/swiper-bundle.esm.js';
 // import 'swiper/swiper-bundle.css';
-var swiper = new Swiper('.swiper', {
+var swiper1 = new Swiper('.swiper1', {
   slidesPerView: 'auto',
   spaceBetween: 30,
   // pagination: {
@@ -9,7 +9,43 @@ var swiper = new Swiper('.swiper', {
   // },
   // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.swiper-button-next1',
+    prevEl: '.swiper-button-prev1',
+  },
+});
+
+var swiper1 = new Swiper('.swiper1', {
+  slidesPerView: 'auto',
+  spaceBetween: 30,
+  navigation: {
+    nextEl: '.swiper-button-next1--02',
+    prevEl: '.swiper-button-prev1--02',
+  },
+});
+
+var swiper2 = new Swiper('.swiper2', {
+  slidesPerView: 'auto',
+  spaceBetween: 30,
+  navigation: {
+    nextEl: '.swiper-button-next2',
+    prevEl: '.swiper-button-prev2',
+  },
+});
+
+var swiper2 = new Swiper('.swiper2', {
+  slidesPerView: 'auto',
+  spaceBetween: 30,
+  navigation: {
+    nextEl: '.swiper-button-next2--02',
+    prevEl: '.swiper-button-prev2--02',
+  },
+});
+
+var swiper3 = new Swiper('.swiper3', {
+  slidesPerView: 'auto',
+  spaceBetween: 30,
+  navigation: {
+    nextEl: '.swiper-button-next3',
+    prevEl: '.swiper-button-prev3',
   },
 });
